9c35beb7bfe91cb30d5d12243d0cdd482c3c320d
Timothy Vladimír Hobbs <timothyhobbs@seznam.cz>
2014-01-08 20:18:24 +0100

    Add copyright notice to two files

--- Parsers/ContinuationParser/Take.elm
+++ Parsers/ContinuationParser/Take.elm
@@ -1,3 +1,6 @@
+{-
+Copyright information can be found at the end of this file.
+-}
 module Parsers.ContinuationParser.Take where
 {-|
 # Lexemes
@@ -85,3 +88,22 @@ If you ask for 5 chars when the remaining input is ['b','y','e'] you get only 3 
 lookAheadInternal: Int -> ContinuationParser input [input] output
 lookAheadInternal n continuation input =
  continuation (take n input) input
+{-
+The continuation parser
+Parsec inspired continuation passing style parser
+
+    Copyright (C) 2013  Timothy Hobbs <timothyhobbs@seznam.cz> thobbs.cz
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3.0 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library.
+-}
--- Parsers/ContinuationParser/Types.elm
+++ Parsers/ContinuationParser/Types.elm
@@ -1,3 +1,6 @@
+{-
+Copyright information can be found at the end of this file.
+-}
 module Parsers.ContinuationParser.Types where
 {-|
 ## Types
@@ -31,3 +34,22 @@ type Continuation input intermediate output
 
 type ContinuationParser input intermediate output
  = (Continuation input intermediate output) -> Parser input output
+{-
+The continuation parser
+Parsec inspired continuation passing style parser
+
+    Copyright (C) 2013  Timothy Hobbs <timothyhobbs@seznam.cz> thobbs.cz
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3.0 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library.
+-}
